fa9ea9770a9eca97cdd124e9c7d8264da38f7d17
ralph <xie@avatarin.com>
2024-06-10 13:59:59 +0900

    update

---
+++ YUV/gen_clips.sh
@@ -0,0 +1,9 @@
+if [ ! -e TestClips ]; then
+  mkdir -p TestClips
+fi
+
+# all black pixels test
+ffmpeg -y -f lavfi -i color=black -pix_fmt yuv420p -fs 0.1k TestClips/blacktest.mp4
+
+# rgb test
+ffmpeg -y -f lavfi -i rgbtestsrc -pix_fmt yuv420p -fs 0.1K TestClips/rgbtest.mp4
