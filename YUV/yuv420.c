d68af86203633a792b652cb9298c3b6e4849de88
ralph <xie@avatarin.com>
2024-06-10 18:26:51 +0900

    update

--- YUV/yuv420.c
+++ YUV/yuv420.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gst/gst.h>
+#include <gst/app/gstappsink.h>
+
+typedef struct _CustomData {
+    GstElement *pipeline;
+    GstElement *source;
+    GstElement *sink;
+    GMainLoop *main_loop;         /* GLib's Main Loop */
+} CustomData;
 
 void usage(char* argv[]) {
     fprintf(stderr, "usage: %s [absolute path of test clip]\n", argv[0]);
     exit(1);
 }
 
+GstFlowReturn new_preroll_cb(GstAppSink* appsink, gpointer user_data) {
+    return GST_FLOW_OK;
+}
+
+GstFlowReturn new_sample_cb(GstAppSink* appsink, gpointer user_data) {
+    printf("on new frame\n");
+    return GST_FLOW_OK;
+}
+
+void app_sink_set_callback(GstElement* sink, CustomData* data) {
+    GstAppSinkCallbacks callbacks;
+    callbacks.eos = NULL;
+    callbacks.new_event = NULL;
+    callbacks.new_preroll = new_preroll_cb;
+    callbacks.new_sample = new_sample_cb;
+
+    gst_app_sink_set_callbacks(GST_APP_SINK(sink), &callbacks, data, NULL);
+}
+
 int real_main (int argc, char *argv[]) {
-    GstElement *pipeline;
     GstBus *bus;
     GstMessage *msg;
+    CustomData data;
 
     if (argc == 1) {
         usage(argv);
@@ -20,26 +47,35 @@ int real_main (int argc, char *argv[]) {
     gst_init (&argc, &argv);
 
     char* path = realpath(argv[1], NULL);
-    char uri[1024] = {0};
-    snprintf(uri, sizeof(uri), "playbin uri=file://%s", path);
+    char pipeline[1024] = {0};
+    snprintf(pipeline, sizeof(pipeline)-1, 
+        "uridecodebin name=src uri=file://%s ! appsink name=sink", path);
+    
+    printf("pipeline = %s\n\n", pipeline);
 
-    /* Build the pipeline */
-    pipeline = gst_parse_launch(uri, NULL);
+    /* Create the empty pipeline */
+    data.pipeline = gst_parse_launch(pipeline, NULL);
+    data.sink = gst_bin_get_by_name(GST_BIN(data.pipeline), "sink");
 
-    // g_signal_connect (data.app_sink, "new-sample", G_CALLBACK (new_sample), &data)
+    if (!data.pipeline || !data.sink) {
+        g_printerr ("Not all elements could be created.\n");
+        return -1;
+    }
+
+    app_sink_set_callback(data.sink, &data);
 
     /* Start playing */
-    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
 
     /* Wait until error or EOS */
-    bus = gst_element_get_bus (pipeline);
+    bus = gst_element_get_bus (data.pipeline);
     msg =
         gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
         GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
     /* See next tutorial for proper error message handling/parsing */
     if (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_ERROR) {
-    g_error ("An error occurred! Re-run with the GST_DEBUG=*:WARN environment "
+        g_error ("An error occurred! Re-run with the GST_DEBUG=*:WARN environment "
         "variable set for more details.");
     }
 
@@ -47,8 +83,8 @@ int real_main (int argc, char *argv[]) {
     gst_message_unref (msg);
     gst_object_unref (bus);
 
-    gst_element_set_state (pipeline, GST_STATE_NULL);
-    gst_object_unref (pipeline);
+    gst_element_set_state (data.pipeline, GST_STATE_NULL);
+    gst_object_unref (data.pipeline);
 
     free(path);
     return 0;
