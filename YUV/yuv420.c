f009d81194ff95a1c6ea4a16fe7479d4a6e2900d
ralph <ralphxwz@gmail.com>
2024-06-12 00:30:15 +0900

    update

--- YUV/yuv420.c
+++ YUV/yuv420.c
@@ -16,11 +16,6 @@ void usage(char* argv[]) {
     exit(1);
 }
 
-
-void on_eos(GstAppSink* appsink, gpointer user_data) {
-
-}
-
 void handle_yuv_sample(GstSample* sample) {
     GstBuffer* buffer = gst_sample_get_buffer(sample);
     if (!buffer) {
@@ -49,6 +44,7 @@ void handle_yuv_sample(GstSample* sample) {
             }
             printf("%4d", data[i]);
         }
+        printf("\n");
         gst_buffer_unref(buffer);
     }
 }
@@ -60,19 +56,15 @@ GstFlowReturn on_new_preroll(GstAppSink* appsink, gpointer user_data) {
 GstFlowReturn on_new_sample(GstAppSink* appsink, gpointer user_data) {
     printf("on new frame\n");
     GstSample* sample = gst_app_sink_pull_sample(appsink);
-    handle_yuv_sample(sample);
-    gst_sample_unref(sample);
-
-    GstAppSinkCallbacks callbacks = {0};
-    gst_app_sink_set_callbacks(appsink, &callbacks, NULL, NULL);
-    // CustomData* data = (CustomData* )user_data;
-    // gst_element_set_state (data->pipeline, GST_STATE_NULL);
+    if (sample) {
+        handle_yuv_sample(sample);
+        gst_sample_unref(sample);
+    }
     return GST_FLOW_OK;
 }
 
 void app_sink_set_callback(GstElement* sink, CustomData* data) {
     GstAppSinkCallbacks callbacks = {0};
-    callbacks.eos = on_eos;
     callbacks.new_preroll = on_new_preroll;
     callbacks.new_sample = on_new_sample;
     gst_app_sink_set_callbacks(GST_APP_SINK(sink), &callbacks, data, NULL);
@@ -80,6 +72,8 @@ void app_sink_set_callback(GstElement* sink, CustomData* data) {
 
 int real_main (int argc, char *argv[]) {
     CustomData data;
+    GstMessage* msg;
+    GstBus* bus;
 
     if (argc == 1) {
         usage(argv);
@@ -88,36 +82,35 @@ int real_main (int argc, char *argv[]) {
     /* Initialize GStreamer */
     gst_init(&argc, &argv);
 
-    char* path = realpath(argv[1], NULL);
     char pipeline[1024] = {0};
     snprintf(pipeline, sizeof(pipeline)-1, 
-        "uridecodebin name=src uri=file://%s ! appsink name=sink", path);
+        "filesrc location=%s ! decodebin ! appsink name=sink", argv[1]);
     
     printf("pipeline = %s\n\n", pipeline);
 
     /* Create the empty pipeline */
     data.pipeline = gst_parse_launch(pipeline, NULL);
-    data.source = gst_bin_get_by_name(GST_BIN(data.pipeline), "src");
+    // data.source = gst_bin_get_by_name(GST_BIN(data.pipeline), "src");
     data.sink = gst_bin_get_by_name(GST_BIN(data.pipeline), "sink");
 
     if (!data.pipeline || !data.sink) {
         g_printerr ("Not all elements could be created.\n");
         return -1;
     }
-
+    
+    gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
     // app_sink_set_callback(data.sink, &data);
 
     /* Start playing */
     gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
     GstSample* sample = gst_app_sink_pull_sample((GstAppSink*)data.sink);
-    handle_yuv_sample(sample);
-    gst_sample_unref(sample);
+    if (sample) {
+        handle_yuv_sample(sample);
+        gst_sample_unref(sample);
+    }
 
-    printf("\n======= done ========\n");
-    // gst_element_send_event(data.pipeline, gst_event_new_eos());
     // gst_element_set_state(data.pipeline, GST_STATE_NULL);
-    // gst_object_unref (data.pipeline);
-    free(path);
+    gst_object_unref (data.pipeline);
     return 0;
 }
 
@@ -125,6 +118,6 @@ int main(int argc, char *argv[]) {
 #if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
   return gst_macos_main ((GstMainFunc) real_main, argc, argv, NULL);
 #else
-  return real_main (argc, argv);
+  return real_main(argc, argv);
 #endif
 }
