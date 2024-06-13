a5c5600798287130ae5795bb990233431671b98a
ralph <xie@avatarin.com>
2024-06-13 16:48:25 +0900

    update

--- FFMpegScript/realrgb.sh
+++ FFMpegScript/realrgb.sh
@@ -1,1 +1,5 @@
-ffmpeg -f lavfi -i color=red -f lavfi -i color=lawngreen -f lavfi -i color=blue -filter_complex 'vstack=3' -f sdl tes
+# ffmpeg -f lavfi -i color=red -f lavfi -i color=lawngreen -f lavfi -i color=blue -filter_complex 'vstack=3' -f sdl test
+
+ffmpeg -f lavfi -i color=red -f lavfi -i color=lawngreen -f lavfi -i color=blue \
+	-filter_complex "[0]scale=320:80[a]; [1]scale=320:80[b]; [2]scale=320:80[c]; [a][b][c]vstack=inputs=3" \
+	-f sdl test
