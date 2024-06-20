5ef01460261208f10d20d0c9c9a55ef307805ead
ralph <ralphxwz@gmail.com>
2024-06-21 01:01:50 +0900

    update

--- FFMpegScript/realrgb.sh
+++ FFMpegScript/realrgb.sh
@@ -1,7 +1,7 @@
 # ffmpeg -f lavfi -i color=red -f lavfi -i color=lawngreen -f lavfi -i color=blue -filter_complex 'vstack=3' -f sdl test
 
 ffmpeg -f lavfi -i color=red -f lavfi -i color=lawngreen -f lavfi -i color=blue \
-	-filter_complex "[0:v]scale=320:80[a]; [1:v]scale=320:80[b]; [2:v]scale=320:80[c]; [a][b][c]vstack=inputs=3" \
+	-filter_complex "[0:v]scale=320:80,setsar=1:1[a]; [1:v]scale=320:80[b]; [2:v]scale=320:80[c]; [a][b][c]vstack=inputs=3" \
 	-f sdl test
 
 
--- FFMpegScript/realrgb2.sh
+++ FFMpegScript/realrgb2.sh
@@ -1,3 +1,3 @@
 # rgb video using padding
 
-ffmpeg -f lavfi -i color=blue -vf 'scale=320:80[1]; [1]pad=320:160:0:80:lawngreen[2]; [2]pad=320:240:0:80:red' -f sdl test
+ffmpeg -f lavfi -i color=blue -vf 'scale=320:80,setsar=1:1[1]; [1]pad=320:160:0:80:lawngreen[2]; [2]pad=320:240:0:80:red' -f sdl test
