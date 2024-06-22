4e8059a78e445d621b24ba87cf3e3b095a159caf
ralph <ralphxwz@gmail.com>
2024-06-22 11:34:13 +0900

    update

--- FFMpegScript/overlay_h.sh
+++ FFMpegScript/overlay_h.sh
@@ -1,1 +1,1 @@
-ffplay -f lavfi -i testsrc -vf 'plit[1][2];[1]pad=iw:ih*2[A];[2]lutrgb=g=256[B];[A][B]overlay=0:h'
+ffplay -f lavfi -i testsrc -vf 'split[1][2];[1]pad=iw:ih*2[A];[2]lutrgb=g=256[B];[A][B]overlay=0:h'
